e4b56e78c64a4978849c47bce96e9410a2aa2fab
Benjamin Graham <grahamfamily15@hotmail.com>
2020-05-04 16:55:21 -0500

    added linq

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -17,85 +17,129 @@ namespace Movies.Pages
         /// <summary>
         /// The current search terms
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; } = "";
 
         /// <summary>
         /// filtered MPAARatings
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
+
         public string[] MPAARatings { get; set; }
 
         /// <summary>
         /// filtered genre
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string[] Genre { get; set; }
 
         /// <summary>
         /// the minimum IMDB rating
         /// </summary>
-        public float IMDBMin { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public double? IMDBMin { get; set; }
 
         /// <summary>
         /// the maximum IMDB rating
         /// </summary>
-        public float IMDBMax { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public double? IMDBMax { get; set; }
 
         /// <summary>
         /// the minimum RottenTomatoesMin rating
         /// </summary>
-        public float RottenTomatoesMin { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public double? RottenTomatoesMin { get; set; }
 
         /// <summary>
         /// the maximum RottenTomatoesMax rating
         /// </summary>
-        public float RottenTomatoesMax { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public double? RottenTomatoesMax { get; set; }
 
         /// <summary>
         /// Gets the search results for display on the page
         /// </summary>
-        public void OnGet(double? IMDBMin, double? IMDBMax, double? RottenTomatoesMin, double? RottenTomatoesMax)
+        public void OnGet()
         {
-            SearchTerms = Request.Query["SearchTerms"];
-            MPAARatings = Request.Query["MPAARatings"];
-            Genre = Request.Query["Genre"];
-
-            //null check
-            if (IMDBMin == null)
-            {
-                this.IMDBMin = 0; //if null, sets as default value
-            }
-            else
-            this.IMDBMin = (float)IMDBMin;
-            if(IMDBMax == null)
-            {
-                this.IMDBMax = 10; //if null, sets as default value
-            }
-            else
-            this.IMDBMax = (float)IMDBMax;
-            //null check
-            if (RottenTomatoesMin == null)
-            {
-                this.RottenTomatoesMin = 0; //if null, sets as default value
-            }
-            else
-                this.RottenTomatoesMin = (float)RottenTomatoesMin;
-            if (RottenTomatoesMax == null)
-            {
-                this.RottenTomatoesMax = 100; //if null, sets as default value
-            }
-            else
-                this.RottenTomatoesMax = (float)RottenTomatoesMax;
-
+            /*
             Movies = MovieDatabase.Search(SearchTerms);
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
             Movies = MovieDatabase.FilterByGenre(Movies, Genre);
             Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
             Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RottenTomatoesMin, RottenTomatoesMax);
+            */
+            Movies = MovieDatabase.All;
+            if (SearchTerms != null)
+            {
+                //Movies = MovieDatabase.All.Where(movie => movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.InvariantCultureIgnoreCase));
+                Movies = from movie in Movies
+                         where movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.InvariantCultureIgnoreCase)
+                         select movie;
+            }
+            if (MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = from movie in Movies
+                         where movie.MPAARating != null && MPAARatings.Contains(movie.MPAARating)
+                         select movie;
+            }
+            if (Genre != null && Genre.Count() != 0)
+            {
+                Movies = from movie in Movies
+                         where movie.MajorGenre != null && Genre.Contains(movie.MajorGenre)
+                         select movie;
+            }
+            if (IMDBMax != null || IMDBMin != null)
+            {
+                if (IMDBMin == null)
+                {
+                    Movies = from movie in Movies
+                             where movie.IMDBRating <= IMDBMax
+                             select movie;
+                }
+
+                else if (IMDBMax == null)
+                {
+
+                    Movies = from movie in Movies
+                             where movie.IMDBRating >= IMDBMin
+                             select movie;
+                }
+
+                else
+                {
+                    Movies = from movie in Movies
+                             where movie.IMDBRating >= IMDBMin && movie.IMDBRating <= IMDBMax
+                             select movie;
+                }
+            }
 
 
 
+            if (RottenTomatoesMax != null || RottenTomatoesMin != null)
+            {
+                if (RottenTomatoesMin == null)
+                {
+                    Movies = from movie in Movies
+                             where movie.RottenTomatoesRating <= RottenTomatoesMax
+                             select movie;
+                }
+                else if (RottenTomatoesMax == null)
+                {
+                    Movies = from movie in Movies
+                             where movie.RottenTomatoesRating <= RottenTomatoesMin
+                             select movie;
+                }
+                else
+                {
+                    Movies = from movie in Movies
+                             where movie.RottenTomatoesRating >= RottenTomatoesMin && movie.RottenTomatoesRating <= RottenTomatoesMax
+                             select movie;
+                }
+            }
         }
+
+
     }
 }
+
